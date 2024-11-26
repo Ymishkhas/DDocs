9d9133862556dd028601f71a979d7fcf504b6f19
Yousef Mishkhas <y3ou12@gmail.com>
2024-11-26 14:48:53 +0300

    add init db scripts

--- db_init_scripts/01_init_db.sql
+++ db_init_scripts/01_init_db.sql
@@ -1,8 +1,35 @@
 -- Create User table
-CREATE TABLE IF NOT EXISTS public."User" (
-    id CHARACTER VARYING(255),
-    user_name CHARACTER VARYING(255) NOT NULL,
-    first_name CHARACTER VARYING(120),
-    last_name CHARACTER VARYING(120),
-    CONSTRAINT "User_pkey" PRIMARY KEY (id)
+CREATE TABLE "Users" (
+    "user_id" varchar(255) PRIMARY KEY,
+    "username" varchar UNIQUE NOT NULL,
+    "first_name" varchar(120) NOT NULL,
+    "last_name" varchar(120) NOT NULL,
+    "email" varchar,
+    "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create Folders Table
+CREATE TABLE "Folders" (
+    "folder_id" serial PRIMARY KEY,
+    "parent_id" int,
+    "user_id" varchar(255),
+    "name" varchar(255) NOT NULL,
+    "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY ("parent_id") REFERENCES "Folders" ("folder_id") ON DELETE CASCADE,
+    FOREIGN KEY ("user_id") REFERENCES "Users" ("user_id") ON DELETE CASCADE
+);
+
+-- Create Files Table
+CREATE TABLE "Files" (
+    "file_id" serial PRIMARY KEY,
+    "folder_id" int,
+    "title" varchar(255) NOT NULL,
+    "description" text,
+    "content" text,
+    "is_public" boolean DEFAULT FALSE,
+    "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY ("folder_id") REFERENCES "Folders" ("folder_id") ON DELETE CASCADE
 );
--- db_init_scripts/02_insert_db.sql
+++ db_init_scripts/02_insert_db.sql
@@ -1,7 +1,16 @@
 -- Populate the database with data
 
-INSERT INTO public."User" (id, user_name, first_name, last_name) VALUES ('dfsfsfy653hg','mali', 'Mohamed', 'Ali');
-INSERT INTO public."User" (id, user_name, first_name, last_name) VALUES ('jfhget65bvj','fhassan', 'Fatima', 'Hassan');
-INSERT INTO public."User" (id, user_name, first_name, last_name) VALUES ('jf753gfdvwk','amahmoud', 'Ahmed', 'Mahmoud');
-INSERT INTO public."User" (id, user_name, first_name, last_name) VALUES ('sdfsdf65rggr', 'sabdullah','Sara', 'Abdullah');
-INSERT INTO public."User" (id, user_name, first_name, last_name) VALUES ('fdsfsdf446ret6', 'yibrahim','Yusuf', 'Ibrahim');
+-- Insert into Users Table
+INSERT INTO public."Users" (user_id, username, first_name, last_name, email) VALUES ('dfsfsfy653hg', 'mali', 'Mohamed', 'Ali', 'mali@example.com');
+INSERT INTO public."Users" (user_id, username, first_name, last_name, email) VALUES ('jfhget65bvj', 'fhassan', 'Fatima', 'Hassan', 'fhassan@example.com');
+INSERT INTO public."Users" (user_id, username, first_name, last_name, email) VALUES ('jf753gfdvwk', 'amahmoud', 'Ahmed', 'Mahmoud', 'amahmoud@example.com');
+
+-- Insert into Folders Table
+INSERT INTO public."Folders" (folder_id, parent_id, user_id, name) VALUES (1, NULL, 'dfsfsfy653hg', 'Main');
+INSERT INTO public."Folders" (folder_id, parent_id, user_id, name) VALUES (2, 1, 'dfsfsfy653hg', 'Src');
+INSERT INTO public."Folders" (folder_id, parent_id, user_id, name) VALUES (3, 1, 'dfsfsfy653hg', 'Components');
+
+-- Insert into Files Table
+INSERT INTO public."Files" (file_id, folder_id, title, description, content, is_public) VALUES (1, 1, 'File 1', 'Description of File 1', 'Content of File 1', FALSE);
+INSERT INTO public."Files" (file_id, folder_id, title, description, content, is_public) VALUES (2, 3, 'File 2', 'Description of File 2', 'Content of File 2', TRUE);
+INSERT INTO public."Files" (file_id, folder_id, title, description, content, is_public) VALUES (3, 3, 'File 3', 'Description of File 3', 'Content of File 3', FALSE);
