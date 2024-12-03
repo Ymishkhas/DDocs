26933657e605cdce9ab48145a442d0f4c279e1dc
Yousef Mishkhas <y3ou12@gmail.com>
2024-12-03 22:54:26 +0300

    fix dynamic display of content

--- react_app/src/App.jsx
+++ react_app/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Home from './components/Home';
@@ -7,22 +8,45 @@ import FileExplorer from './components/FileExplorer';
 import Content from './components/Content';
 import './App.css';
 
+function Main() {
+  const location = useLocation();
+  const mainContainerRef = useRef(null);
+
+  useEffect(() => {
+    const mainContainer = mainContainerRef.current;
+    if (mainContainer) {
+      const childCount = mainContainer.childElementCount;
+      if (childCount === 1) {
+        mainContainer.classList.add('single-item');
+        mainContainer.classList.remove('two-items');
+      } else if (childCount === 2) {
+        mainContainer.classList.add('two-items');
+        mainContainer.classList.remove('single-item');
+      }
+    }
+  }, [location]);
+
+  return (
+    <div id='main' ref={mainContainerRef}>
+      <ProtectedRoute>
+          <FileExplorer />
+      </ProtectedRoute>
+      <div id='content'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:file_id" element={<Content />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Header />
-        <div id='main'>
-          <ProtectedRoute>
-            <FileExplorer />
-          </ProtectedRoute>
-          <div id='content'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/:file_id" element={<Content />} />
-            </Routes>
-          </div>
-        </div>
+        <Main />
       </AuthProvider>
     </BrowserRouter>
   );
